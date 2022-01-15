8d828fb02a9d9f17d6e155121d4f82139110c87e
rajat <rkumar201@sapient.com>
2022-01-15 06:07:33 -0600

    created the layout and elements for sudoku board

--- app.js
+++ app.js
@@ -1,1 +1,52 @@
-let hello = "1"
+// Introducing an IIFE for avoiding pollution in global namespace
+(function(fetchTool) {
+    const puzzleContainer = document.querySelector("#puzzle-container");
+    const solvePuzzleBtn = document.querySelector("#solve-puzzle-btn");
+
+    const puzzleSquares = 81;
+
+    let sudokuValues = [];
+
+    const clearSudokuValues = () => {
+        sudokuValues = [];
+    }
+
+    // This method 
+    const joinSudokuValues = () => {
+        clearSudokuValues();
+
+        const allInputs = document.querySelectorAll("input");
+
+        allInputs.forEach( input => {
+            if(input.value) {
+                sudokuValues.push(input.value);
+            } else {
+                sudokuValues.push(".");
+            }
+        });
+
+        console.log(sudokuValues);
+    };
+
+    const solveSudoku = () => {
+
+    };
+
+    // added click listener for solve button
+    solvePuzzleBtn.addEventListener("click", joinSudokuValues);
+
+    // loop over squares to create squares numbered inputs
+    for (let i = 0; i< puzzleSquares; i++) {
+        const inputEl = document.createElement("input");
+        inputEl.setAttribute("type", "number");
+        inputEl.setAttribute("min", 0);
+        inputEl.setAttribute("max", 9);
+    
+        puzzleContainer.appendChild(inputEl);
+    }
+})(axios);
+
+
+
+
+
